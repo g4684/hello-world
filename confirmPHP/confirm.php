a62304d1e5123d7c69e19cc9ea80f3cb86907f1f
g4684 <60440523+g4684@users.noreply.github.com>
2020-11-10 11:54:43 -0500

    Add files via upload

---
+++ confirmPHP/confirm.php
@@ -0,0 +1,125 @@
+<html>
+<head>
+  <meta charset=”utf-8”>  
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <!-- <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+  <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin> </script> -->
+  <title>Mega Travel Contact Agent</title>
+  <link href="styles/MegaTravel.css" rel="stylesheet" /> 
+  
+</head>  
+<body>
+<header>
+    <img src="images/megatravellogo.png" alt="" width="400" height="225" style="float:left" />
+  </header>
+
+  <div id="WelcomeBar"></div>
+  
+  <nav>
+    <ul>
+        <li><a href="MegaTravel.html">Home</a></li>
+        <li><a href="AboutUs.html">About Us</a></li>
+        <li><a href="Contact.html">Contact Agent</a></li>
+    </ul>
+   </nav>
+
+<?php
+
+$activities = array();
+
+if ( isset( $_POST['museums'] ) ) {
+  $museums = "Museums";
+  array_push($activities,"Museums");
+}
+
+if ( isset( $_POST['sailing'] ) ) {
+  $sailing = "Sailing";
+  array_push($activities,"Sailing");
+}
+
+if ( isset( $_POST['beach'] ) ) {
+  $beach = "Beach";
+  array_push($activities,"Beach");
+}
+
+if ( isset( $_POST['hiking'] ) ) {
+  $hiking = "Hiking";
+  array_push($activities,"Hiking");
+}
+
+if ( isset( $_POST['boating'] ) ) {
+  $sailing = "Boating";
+  array_push($activities,"Boating");
+}
+
+if ( isset( $_POST['cityTours'] ) ) {
+  $sailing = "City Tours";
+  array_push($activities,"City Tours");
+}
+
+if ( isset( $_POST['sports'] ) ) {
+  $sailing = "Sports";
+  array_push($activities,"Sports");
+}
+
+if ( isset( $_POST['cycling'] ) ) {
+  $sailing = "Cycling";
+  array_push($activities,"Cycling");
+}
+
+if ( isset( $_POST['theatre'] ) ) {
+  $sailing = "Theatre";
+  array_push($activities,"Theatre");
+}
+
+if ( isset( $_POST['parksRec'] ) ) {
+  $sailing = "Parks and Recreation";
+  array_push($activities,"Parks and Recreation");
+}
+
+if ( isset( $_POST['snorkeling'] ) ) {
+  $sailing = "Snorkeling";
+  array_push($activities,"Snorkeling");
+}
+
+
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$userPhone = $_POST["userPhone"];
+$userEmail = $_POST["userEmail"];
+$adults = $_POST["adults"];
+$children = $_POST["children"];
+$travelDate = $_POST["travelDate"];
+$destination = $_POST["destination"];
+
+?>
+<section class="intro">
+<H3>Thank you for submitting your travel agent contact request! Here is the information you submitted:</H3>
+<br>
+<div>
+    <b>Client Name:</b> <?php echo $firstname . " " . $lastname; ?><br>
+    <b>Client Phone Number:</b> <?php echo $userPhone; ?><br>
+    <b>Client Email:</b> <?php echo $userEmail; ?><br>
+    <b>Number of Adults:</b> <?php echo $adults; ?><br>
+    <b>Number of Children:</b> <?php echo $children; ?><br>
+    <b>Travel Date:</b> <?php echo $travelDate; ?><br>
+    <b>Destination:</b> <?php echo $destination; ?><br>
+    <b>Interested Activities:</b><br> 
+    <?php
+    foreach($activities as $value){
+      echo $value . "<br>";
+    }
+    ?>
+</div>
+<br>
+<p>An agent will be in touch with you soon!</p>
+</section>    
+<br>
+<footer>
+      <p style="color:white; background-color: black;">Copyright Protected. All rights reserved.<br><br>
+      MEGA TRAVELS<br>mega@Travels.com</p>
+</footer>
+</body>
+</html>
+
+
